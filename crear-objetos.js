7b8456ee15b302d6f6790b098d70b565b4ca4ec8
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-04-01 16:04:45 -0500

    creando moldes

---
+++ crear-objetos.js
@@ -0,0 +1,17 @@
+//confirguran las propiedades del objeto, clase o molde.
+class Cliente {
+ nombreCliente;
+ dniCliente;
+ numeroCuenta;
+ saldoCuenta;
+}
+
+const cliente1 = new Cliente(); //asignando un molde
+
+cliente1.nombreCliente = "Nathalia";
+cliente1.dniCliente = "1.144.166.551"
+cliente1.numeroCuenta = "32656232";
+cliente1.saldoCuenta = "$521368";
+cliente1.
+
+console.log(cliente1);
