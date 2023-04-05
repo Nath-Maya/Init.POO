bb63d0aa1631b4b268a1f03b3fdc7fdafe4215a4
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-04-04 22:35:38 -0500

    Retornando valores

--- 04-metodoRetorno.js
+++ 04-metodoRetorno.js
@@ -21,20 +21,24 @@ class cliente {
    depositoCuenta(valor) {
      if (valor > 0) //solo recibe depositos positivos.
      this.#saldoCuenta += valor;
+     return this.#saldoCuenta;
    }
  
    retirarDeCuenta(valor) {
      if (valor <= this.#saldoCuenta) //no permite que la cuenta quede con saldo negativo. 
      this.#saldoCuenta = this.#saldoCuenta - valor;
    }
+
+   verSaldo() {
+    return this.#saldoCuenta;
+  }
+
  }
  
  cuentaDeNathalia = new cuentaCorriente();
  
- //le decimos a JS un valor inicial numerido
- 
- // cuentaDeNathalia.#saldoCuenta = 0;
- 
- cuentaDeNathalia.depositoCuenta(100), console.log(cuentaDeNathalia);
- cuentaDeNathalia.retirarDeCuenta(1000), console.log(cuentaDeNathalia);
- 
+let saldoCuenta = cuentaDeNathalia.verSaldo();
+console.log('El saldo actual es ' + saldoCuenta);
+ cuentaDeNathalia.depositoCuenta(100)
+
+ cuentaDeNathalia.retirarDeCuenta(100)
