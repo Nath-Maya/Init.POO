f04fdeadd553a328680e0774cf702ea8dc7cf7a8
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-04-05 00:02:21 -0500

    Ajustando nodemon

--- aula3/index.js
+++ aula3/index.js
@@ -5,8 +5,9 @@ import {cuentaCorriente} from './cuentaCorriente.js';
 const cuentaDeNathalia = new cuentaCorriente();
  
 
- cuentaDeNathalia.depositoCuenta(250);
+ cuentaDeNathalia.depositoCuenta(350);
+ cuentaDeNathalia.retirarDeCuenta(200);
  
  let saldoCuenta = cuentaDeNathalia.verSaldo();
 //  cuentaDeNathalia.retirarDeCuenta(50);
- console.log('El saldo actual es ' + saldoCuenta);
+ console.log('El saldo actual es el siguiente: ' + saldoCuenta);
