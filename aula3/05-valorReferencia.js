d4342485659f55feee78935a0be86a6104ab256a
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-04-07 10:51:36 -0500

    Revert "Revert "Create 05-valorReferencia.js""
    This reverts commit b0038253d7dc305e8a1e1158f583d8ae9c4f2f89.

---
+++ aula3/05-valorReferencia.js
@@ -0,0 +1,12 @@
+
+//! Parametros por valor y por referencia
+
+let valor = "El resultado de algo o valor";
+let referencia = "puede ser otro objeto o funcion";
+
+function parametros(valor,referencia) {
+   console.log(valor,referencia)
+};
+
+
+
