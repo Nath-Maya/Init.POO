d0776576da46e0798f93e5b920568ac8e4a5d845
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-04-26 19:27:10 -0500

    Se crea archivo Cuenta y se realiza la extension de la cuentacorriente y ahorros

--- herencia-polimorfismo/CuentaCorriente.js
+++ herencia-polimorfismo/CuentaCorriente.js
@@ -1,50 +1,14 @@
 import { Cliente } from "./Cliente.js";
+import { Cuenta } from "./Cuentas.js";
 
-export class CuentaCorriente
-{
-    #cliente;
-    numero;
-    agencia;
-    #saldo;
+//!HERENCIA
+
+export class CuentaCorriente extends Cuenta {
+    //exporte la clase Cuenta corriente que extiende de la clase Cuenta, es decir que, cuentaCorriente es hija de Cuenta.  
+
     static cantidadCuentas = 0;
 
-    set cliente(valor) {
-        if (valor instanceof Cliente)
-            this.#cliente = valor;
-    }
-
-    get cliente() {
-        return this.#cliente;
-    }
-
     constructor(cliente, numero, agencia) {
-        this.cliente = cliente;
-        this.numero = numero;
-        this.agencia = agencia;
-        this.#saldo = 0;
         CuentaCorriente.cantidadCuentas++;
-    }
-
-    depositoEnCuenta(valor) {
-        if (valor > 0)
-            this.#saldo += valor;
-        return this.#saldo;
-    }
-
-    retirarDeCuenta(valor) {
-        if (valor <= this.#saldo)
-            this.#saldo -= valor;
-        return this.#saldo;
-    }
-
-    verSaldo() {
-        return this.#saldo;
-    }
-
-    transferirParaCuenta(valor,cuentaDestino) {
-        this.retirarDeCuenta(valor);
-        cuentaDestino.depositoEnCuenta(valor);
-        valor = 200;
-        valor = valor*1000;
     }
 }
--- herencia-polimorfismo/index.js
+++ herencia-polimorfismo/index.js
@@ -1,29 +1,21 @@
-/*Importación de clases*/
+//*Importación de clases
+
 import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
+import {Cuenta} from './Cuentas.js'
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const cuentaDeLeonardo = new Cuenta(cliente, '1', '001',0);
+const cuentaDeMaria = new Cuenta(cliente2,'2','002',0);
 
-let saldo = cuentaDeLeonardo.verSaldo(); 
+const CuentaAhorroLeonardo = new Cuenta('Leonardo','13804050','123224',0);
 
-saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
-
-const saldoMaria = cuentaDeMaria.verSaldo();
-
-console.log('Cuenta de Maria',cuentaDeMaria);
-
-console.log('El Saldo actual (cuentaMaria) '+saldoMaria);
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
-console.log(CuentaCorriente.cantidadCuentas);
-
-console.log(CuentaCorriente.cantidadCuentas);
+console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(150);
+console.log(cuentaDeLeonardo.verSaldo());
+cuentaDeLeonardo.retirarDeCuenta(30);
+console.log(cuentaDeLeonardo.verSaldo());
 
 
 
